b154add85ff1cd2ba738202c79b851e89d48aca7
Adriano José da Silva <adriano.silva@modalgr.com.br>
2022-08-14 23:02:17 -0300

    adicionando a parte do blog

---
+++ blog/blog.js
@@ -0,0 +1,10 @@
+        const sr = ScrollReveal({
+            origin: 'top',
+            distance: '50px',
+            duration: 900,
+            reset: true
+        });
+        ScrollReveal().reveal('.container-post', { delay: 100 });
+        ScrollReveal().reveal('.date', { delay: 200 });
+        ScrollReveal().reveal('.title', { delay: 300 });
+        ScrollReveal().reveal('.description', { delay: 400 });
